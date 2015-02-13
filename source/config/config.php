9ae33ca0a2cb85ecf7f5b6f94dd4d84c1615f66a
arjen_schumacher <jouranoos@gmail.com>
2015-02-13 08:14:44 +0100

    Version 0.4.0.2

--- source/config/config.php
+++ source/config/config.php
@@ -34,7 +34,7 @@ $config->set('ip.address.visitor', $_SERVER['REMOTE_ADDR']);
 // Developer configuration
 $config->set('dev.debug', false);
 $config->set('dev.starttime', microtime());
-$config->set('dev.version', '0.4.0.1');
+$config->set('dev.version', '0.4.0.2');
 
 if ($config->get('dev.debug') === true) {
     error_reporting(E_ALL);
